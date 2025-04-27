2107a19b202fdaca9bdc2764c0564b79cc8908ca
maslo0396 <ilya97750@gmail.cm>
2025-04-27 19:04:08 +0300

    edits-swiper

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './index.html';
 import './index.scss';
+import './swiper-bundle';
 
+const swiper1 = new Swiper('.stickers-swiper', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 70,
+  
+    // If we need pagination
+    pagination: {
+      el: '.stickers-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+  });
+
+  
